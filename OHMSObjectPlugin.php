f08930acab426dd647b5c2be9359654a48225d45
libmanuk <eweig@email.uky.edu>
2016-11-02 20:22:10 -0400

    Update OHMSObjectPlugin.php

--- OHMSObjectPlugin.php
+++ OHMSObjectPlugin.php
@@ -9,7 +9,7 @@ class OHMSObjectPlugin extends Omeka_Plugin_AbstractPlugin
     protected $_filters = array('insertOHMSObject' => array('Display', 'Item', 'Item Type Metadata', 'OHMS Object'));
 
 
-    public function insertOHMSObject($text, $args)
+    public function insertOHMSObject($text)
     {
      return "<div class=\"item-relation\"><p><iframe src=\"" . $text . "\" width=\"100%\" height=\"1110px\" frameborder=\"0\" name=\"viewer\" scrolling=\"no\"></iframe></p></div>";
     }
