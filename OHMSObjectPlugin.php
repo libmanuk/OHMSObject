87870ba41b57d12ebbf9c8ef59b25e88fdd2e851
libmanuk <eweig@email.uky.edu>
2016-11-05 19:23:15 -0400

    Update OHMSObjectPlugin.php

--- OHMSObjectPlugin.php
+++ OHMSObjectPlugin.php
@@ -7,19 +7,26 @@
 
 class OHMSObjectPlugin extends Omeka_Plugin_AbstractPlugin
 {
+    protected $_hooks = array('public_head');
     protected $_filters = array('insertOHMSObject' => array('Display', 'Item', 'Item Type Metadata', 'OHMS Object'));
+
+    public function hookPublicHead($args)
+    {
+        queue_css_file('ohmsobject'); 
+    }
 
     public function insertOHMSObject($text, $args)
     {
         $theme = is_admin_theme() ? 'admin' : 'public';
         $format = metadata('item', array('Item Type Metadata', 'Interview Format'));
+        
 
      if ($theme == 'admin') {
             return "<div class=\"ohms-item\"><p><a href=\"" . $text . "\" target=\"_blank\">" . $text . "</a></p></div>";
                 } else {
             return "<div class=\"ohms-item-" . $format . "\"><p><iframe src=\"" . $text . "\" width=\"100%\" height=\"100%\" frameborder=\"0\" name=\"viewer\" scrolling=\"no\"></iframe></p></div>";
                 }
-        
+
     }
 
 }
