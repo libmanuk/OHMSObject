225e9795a8228cd51b7f54bd3d4d7df6d64ae19a
libmanuk <eweig@email.uky.edu>
2016-11-05 06:09:29 -0400

    Update OHMSObjectPlugin.php

--- OHMSObjectPlugin.php
+++ OHMSObjectPlugin.php
@@ -1,17 +1,23 @@
 <?php
 
 /**
- * OHMS Object plugin.
+ * OHMS Object Linker plugin.
  */
 class OHMSObjectPlugin extends Omeka_Plugin_AbstractPlugin
 {
 
     protected $_filters = array('insertOHMSObject' => array('Display', 'Item', 'Item Type Metadata', 'OHMS Object'));
 
-
     public function insertOHMSObject($text)
     {
-     return "<div class=\"item-relation\"><p><iframe src=\"" . $text . "\" width=\"100%\" height=\"1110px\" frameborder=\"0\" name=\"viewer\" scrolling=\"no\"></iframe></p></div>";
+        $key = is_admin_theme() ? 'admin' : 'public';
+
+        if ($key == 'admin') {
+            return "<div class=\"ohms-item\"><p><a href=\"" . $text . "\" target=\"_blank\">" . $text . "</a></p></div>";
+                } else {
+            return "<div class=\"ohms-item\"><p><iframe src=\"" . $text . "\" width=\"100%\" height=\"100%\" frameborder=\"0\" name=\"viewer\" scrolling=\"no\"></iframe></p></div>";
+                }
+
     }
 
 }
