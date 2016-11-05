db305e5c9e88abd6a59331aed8c729b1a0d567f8
libmanuk <eweig@email.uky.edu>
2016-11-05 19:38:15 -0400

    Update OHMSObjectPlugin.php

--- OHMSObjectPlugin.php
+++ OHMSObjectPlugin.php
@@ -23,7 +23,7 @@ class OHMSObjectPlugin extends Omeka_Plugin_AbstractPlugin
      if ($theme == 'admin') {
             return "<div class=\"ohms-item\"><p><a href=\"" . $text . "\" target=\"_blank\">" . $text . "</a></p></div>";
                 } else {
-            return "<div class=\"ohms-item-" . $format . "\"><p><iframe src=\"" . $text . "\" width=\"100%\" height=\"100%\" frameborder=\"0\" name=\"viewer\" scrolling=\"no\"></iframe></p></div>";
+            return "<div class=\"ohms-item-" . $format . "\"><iframe src=\"" . $text . "\" name=\"viewer\" scrolling=\"no\"></iframe></div>";
                 }
 
     }
